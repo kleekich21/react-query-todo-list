d68a5aeb6430b5b246a14995549de8c8fe0581fb
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-06-12 15:11:38 +0900

    feat: fetching by react-queryu and rendering for detail is implemented

--- src/components/TodoDetails.tsx
+++ src/components/TodoDetails.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { useQuery } from "react-query";
 import { Todo } from "../types/todos";
 
-const fetchTodoById = async (id: string): Promise<Todo> => {
+const fetchTodoById = async (id: number): Promise<Todo> => {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/todos/${id}`
   );
@@ -11,8 +12,24 @@ const fetchTodoById = async (id: string): Promise<Todo> => {
   return response.json();
 };
 
-const TodoDetail: React.FC = () => {
-  return <div>TodoDetail</div>;
+interface TodoDetailProps {
+  todoId: number;
+}
+
+const TodoDetail: React.FC<TodoDetailProps> = ({ todoId }) => {
+  const { data, error, isLoading } = useQuery<Todo, Error>(
+    ["todo", todoId],
+    () => fetchTodoById(todoId)
+  );
+
+  if (isLoading) return <div>Loding...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  return (
+    <div>
+      <h2>{data?.title}</h2>
+      <p>{data?.completed ? "Completed" : "Not Completed"}</p>
+    </div>
+  );
 };
 
 export default TodoDetail;
