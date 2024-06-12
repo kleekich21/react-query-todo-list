7f0a628d0e0355cd6a257f69d13a6b95c3aae1b5
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-06-12 14:14:51 +0900

    feat: addTodo function to create a new todo is added

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,4 +1,21 @@
 import React from "react";
+import { Todo } from "../types/todos";
+
+const addTodo = async (newTodo: Omit<Todo, "id">): Promise<Todo> => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newTodo),
+  });
+
+  if (!response.ok) {
+    throw new Error("Error: Creating new todo is failed");
+  }
+
+  return response.json();
+};
 
 const AddTodo: React.FC = () => {
   return <div>Add Todo</div>;
