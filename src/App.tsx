a4205badb9227d3666fb47ddc2aea4e6f3b205bf
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-06-12 14:06:00 +0900

    feat: boilerplate file for  AddTodo component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import TodoList from "./components/TodoList";
+import AddTodo from "./components/AddTodo";
 
 function App() {
   return (
     <div>
+      <h1>Todo List</h1>
+      <AddTodo />
       <TodoList />
     </div>
   );
