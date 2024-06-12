973a4be15c26a0bd87d9c502a664008692289d9f
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-06-12 13:53:50 +0900

    feat: define a function to call an api to fetch todo list

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const fetchTodos = async (): Promise<Todo[]> => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  if (!response.ok) {
+    throw new Error("Error Fetching Todo List");
+  }
+  return response.json();
+};
+
 function TodoList() {
   return <div>TodoList</div>;
 }
