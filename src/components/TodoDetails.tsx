2752a6d4919b94d02e2fb59af7af34b8da8bda55
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-06-12 15:06:17 +0900

    feat: function to fetch todo by id is created

--- src/components/TodoDetails.tsx
+++ src/components/TodoDetails.tsx
@@ -1,4 +1,15 @@
 import React from "react";
+import { Todo } from "../types/todos";
+
+const fetchTodoById = async (id: string): Promise<Todo> => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${id}`
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+};
 
 const TodoDetail: React.FC = () => {
   return <div>TodoDetail</div>;
