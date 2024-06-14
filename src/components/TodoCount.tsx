0e775dbbdbf4aa5cbf4e2c4d5df07fcc0853d09a
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-06-14 16:18:25 +0900

    refactor: apply customHooks for todoCount along side manged query keys.

--- src/components/TodoCount.tsx
+++ src/components/TodoCount.tsx
@@ -1,23 +1,11 @@
 import React from "react";
-import { Todo } from "../types/todos";
-import { useQuery } from "@tanstack/react-query";
-
-const fetchTodos = async (): Promise<Todo[]> => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-  if (!response.ok) {
-    throw new Error("Error Fetching Todo List");
-  }
-  return response.json();
-};
+import { useTodoList } from "../queries/todos";
 
 const TodoCount: React.FC = () => {
-  const { data, error, isPending, isError } = useQuery({
-    queryKey: ["todos"],
-    queryFn: fetchTodos,
-  });
+  const { data, error, isPending, isError } = useTodoList();
 
   if (isPending) return <div>Loading...</div>;
-  if (isError) return <div>Error: {error.message}</div>;
+  if (isError) return <div>Error: {error?.message}</div>;
   return <div>Total Todos: {data?.length}</div>;
 };
 
