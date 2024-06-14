579550555e04714b631c6f41badd5c38a894c418
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-06-14 16:16:43 +0900

    refactor: apply customHooks for todoDetail along side manged query keys.

--- src/components/TodoDetails.tsx
+++ src/components/TodoDetails.tsx
@@ -1,26 +1,12 @@
 import React from "react";
-import { useQuery } from "@tanstack/react-query";
-import { Todo } from "../types/todos";
-
-const fetchTodoById = async (id: number): Promise<Todo> => {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/todos/${id}`
-  );
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
-  }
-  return response.json();
-};
+import { useTodoDetail } from "../queries/todos";
 
 interface TodoDetailProps {
   todoId: number;
 }
 
 const TodoDetail: React.FC<TodoDetailProps> = ({ todoId }) => {
-  const { data, error, isPending, isError } = useQuery({
-    queryKey: ["todo", todoId],
-    queryFn: () => fetchTodoById(todoId),
-  });
+  const { data, error, isPending, isError } = useTodoDetail(todoId);
 
   if (isPending) return <div>Loding...</div>;
   if (isError) return <div>Error: {error.message}</div>;
